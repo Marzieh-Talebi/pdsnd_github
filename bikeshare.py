75480d8da84f1803014ccf375ea79959305488c6
Marzieh Talebi <marziehtalebi12345@gmail.com>
2021-07-11 19:57:21 +0800

    remove unused code

--- bikeshare.py
+++ bikeshare.py
@@ -60,7 +60,7 @@ def load_data(city, month, day):
     if day != 'all':
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
-    #print(df)
+
     return df
 
 
